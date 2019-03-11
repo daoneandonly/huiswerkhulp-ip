c4058933fe36113ad7f6c656d3f0e5f74468baf0
Steven Whitton <stevenawhitton@gmail.com>
2019-03-11 16:38:42 +0100

    Added forloop content

---
+++ forloop/js/script.js
@@ -0,0 +1,8 @@
+// declareer een array met namen
+var listOfNames = ["Steven", "Folkert-Jan", "Bas", "Dorus", "Titus", "Walter"];
+
+// start for loop, die loopt zolang i < listOfNames.length
+for (var i = 0; i < listOfNames.length; i++) {
+  // console.log() de i-ste uit listOfNames
+  console.log(listOfNames[i]);
+}
