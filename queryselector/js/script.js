19a1022c119168c77fffbb977118ece9ef6394a0
Steven Whitton <stevenawhitton@gmail.com>
2019-03-11 16:45:55 +0100

    Added queryselector content

---
+++ queryselector/js/script.js
@@ -0,0 +1,7 @@
+// declareer variabelen
+var title = document.querySelector("h1");
+var list = document.querySelectorAll("p");
+
+// console.log() title en list[0]
+console.log(title);
+console.log(list[0]);
