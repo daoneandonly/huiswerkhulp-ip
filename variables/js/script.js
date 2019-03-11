337c335016fdb7faf8f1842660e49cb68f76f773
Steven Whitton <stevenawhitton@gmail.com>
2019-03-11 16:18:08 +0100

    Fixed consistency in language

--- variables/js/script.js
+++ variables/js/script.js
@@ -1,10 +1,10 @@
 // declareer variablen
 var text = "My name is";
 var name = "Steven";
-var nummers = ["Een", "twee", 3];
+var numbers = ["Een", "twee", 3];
 
 //stuur de variable text + ": " + name naar de console
 console.log(text + ": " + name);
 
 //stuur de array nummers
-console.log(nummers);
+console.log(numbers);
