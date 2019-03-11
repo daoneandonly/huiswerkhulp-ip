150d4475ff8de0fd17c0cf72c09c6124a84c2678
Steven Whitton <stevenawhitton@gmail.com>
2019-03-11 16:12:46 +0100

    Added Variables content

---
+++ variables/js/script.js
@@ -0,0 +1,10 @@
+// declareer variablen
+var text = "My name is";
+var name = "Steven";
+var nummers = ["Een", "twee", 3];
+
+//stuur de variable text + ": " + name naar de console
+console.log(text + ": " + name);
+
+//stuur de array nummers
+console.log(nummers);
