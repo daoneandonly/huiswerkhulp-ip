63f9ddedf93cfebb014ef65e1b034c9ed57f7b2c
Steven Whitton <stevenawhitton@gmail.com>
2019-03-11 16:22:26 +0100

    Added functions content

---
+++ functions/js/script.js
@@ -0,0 +1,11 @@
+// delcareer variable 'name'
+var name = "Steven";
+
+// declareer function met naam 'sendToConsole' met parameter 'value'
+function sendToConsole(value) {
+  // voer function console.log() uit met daarin de parameter 'value'
+  console.log(value);
+}
+
+// voer function sendToConsole uit en geef de waarde 'name' mee
+sendToConsole(name);
